a1c46dd11827c5a86cd65c0330a0154095e30169
Abhishek  R Prasad <97088891+Abhishek32971@users.noreply.github.com>
2022-02-09 03:53:42 +0000

    .

---
+++ p3original.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int input_number()
+{
+  int a;
+  printf("enter the number\n");
+  scanf("%d",&a);
+  return a;
+}
+int is_composite(int n)
+{
+  int sum=0;
+  for (int i=2;i<n;i++)
+  {
+    sum=sum+(n%i);
+  }
+  return sum;
+}
+void output(int n, int composite)
+{
+  if (composite!=0)
+    printf("%d is a composite number\n",n);
+  else 
+    printf("%d is not a composite number\n",n);
+}
+int main()
+{
+  int n,cmp;
+  n=input_number();
+  cmp=is_composite(n);
+  output(n,cmp);
+  return 0;
+}
